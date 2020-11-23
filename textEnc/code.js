aa82a1bb4adbe1664b8565a907d8da7a1a7d6cd0
mahmed94dev <74860679+mahmed94dev@users.noreply.github.com>
2020-11-23 09:18:04 +0200

    new fix to copy

--- textEnc/code.js
+++ textEnc/code.js
@@ -15,7 +15,14 @@ $(document).ready(function () {
 	
 	document.getElementById("copyButton").addEventListener("click", function() {
     copyToClipboard(document.getElementById("resultAction"));
+	
+	
 });
+
+document.getElementById("copyButton").addEventListener("click", function() {
+	$("#copyAction").text("Copied !");
+		
+	});
 // new code
 function copyToClipboard(elem) {
 	  // create hidden text element, if it doesn't already exist
